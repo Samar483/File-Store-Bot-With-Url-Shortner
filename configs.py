6980599fe24d196cb1711266920849d241562130
Samar483 <119998052+Samar483@users.noreply.github.com>
2024-01-31 17:31:04 +0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  API_ID = int(os.environ.get("API_ID", "24160156"))
+  API_HASH = os.environ.get("API_HASH", "c4287ddea38c7c7d0d42a8321cc9d81b")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6935146097:AAHk2BpqOC2qVwy0CzsHun-qlXmRpLRUPcs")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "PKSaveBot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002054737045"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "krownlinks.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "e6da83c32036d3a4b469d1f60f740dc5192d0885")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1090286526"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://chsamarzulfiqar1:KcUyQjNKEZSjr0fC@cluster0.i2ocfif.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002054737045"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -19,7 +19,7 @@ class Config(object):
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link Join Our Main Channel @CSZMovies. 
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
@@ -32,7 +32,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/CSZMovies)
  
  I am Super noob Please Support My Hard Work.
 
